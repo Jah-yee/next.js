a2bbe144529c3beffc939c3a8f386160916fce9a
Josh Story <story@hey.com>
2026-04-03 15:06:11 -0700

    Allow for parsing app route strings with and without slot names.
    The old impl has a boolean arg that was always true. To support some fixes to routing I need to make the parser sometimes allow for slots but still not things like route groups. I am generalizing the function implementation to have a filter type that decides whether the input string is allowed to have certain types such as route groups and slot names. All the existing uses still disallow any non-url path segments.

--- packages/next/src/server/request/fallback-params.ts
+++ packages/next/src/server/request/fallback-params.ts
@@ -3,7 +3,7 @@ import type { FallbackRouteParam } from '../../build/static-paths/types'
 import type { DynamicParamTypesShort } from '../../shared/lib/app-router-types'
 import { dynamicParamTypes } from '../app-render/get-short-dynamic-param-type'
 import type AppPageRouteModule from '../route-modules/app-page/module'
-import { parseAppRoute } from '../../shared/lib/router/routes/app'
+import { parseNormalizedAppRoute } from '../../shared/lib/router/routes/app'
 import { extractPathnameRouteParamSegmentsFromLoaderTree } from '../../build/static-paths/app/extract-pathname-route-param-segments-from-loader-tree'
 
 export type OpaqueFallbackRouteParamValue = [
@@ -87,7 +87,7 @@ export function getFallbackRouteParams(
   page: string,
   routeModule: AppPageRouteModule
 ) {
-  const route = parseAppRoute(page, true)
+  const route = parseNormalizedAppRoute(page)
 
   // Extract the pathname-contributing segments from the loader tree. This
   // mirrors the logic in buildAppStaticPaths where we determine which segments
